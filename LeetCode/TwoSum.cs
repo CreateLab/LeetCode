e8df844a93ff5f627a0cb69455efba45d5301578
Lary <r.sorokin@reo.ru>
2022-08-09 16:13:34 +0300

    #add 1
    Runtime: 176 ms, faster than 87.19% of C# online submissions for Two Sum.
    Memory Usage: 43.8 MB, less than 22.48% of C# online submissions for Two Sum.

---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode;
+
+/// <summary>
+///  https://leetcode.com/problems/two-sum/
+/// </summary>
+public class TwoSumClass
+{
+    private class Data
+    {
+        public int FirstValue { get; set; }
+        public int SecondValue { get; set; }
+    }
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, Data>(nums.Length);
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(nums[i]))
+            {
+                var data = dict[nums[i]];
+                data.SecondValue = i;
+            }
+            else
+            {
+                dict.Add(nums[i], new Data { FirstValue = i, SecondValue = -1 });
+            }
+        }
+
+        var first = 0;
+        var second = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var tmp = target - nums[i];
+            if (!dict.ContainsKey(tmp)) continue;
+            first = i;
+            var data = dict[tmp];
+            second = data.FirstValue;
+            if (first == second)
+            {
+                if (data.SecondValue == -1)
+                {
+                    continue;
+                }
+
+                second = data.SecondValue;
+            }
+
+            break;
+        }
+
+        return new int[] { first, second };
+    }
+}
---
+++ Leetcode.Tests/TwoSum.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode;
+
+namespace Leetcode.Tests;
+
+public class TwoSum
+{
+    
+    //Runtime: 176 ms, faster than 87.19% of C# online submissions for Two Sum.
+    //Memory Usage: 43.8 MB, less than 22.48% of C# online submissions for Two Sum.
+    [Theory]
+    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+    [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+    [InlineData(new int[] { 3, 3 }, 6, new int[] { 0,1 })]
+    public void TwoSumTest(int[] nums, int target, int[] expected)
+    {
+        var ts = new TwoSumClass();
+        var result = ts.TwoSum(nums, target);
+        Assert.Equal(expected, result);
+    }
+}
